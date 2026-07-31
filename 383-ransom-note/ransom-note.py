74033a6cdb27abeae3786fca965e68e563596111
Ananya <arimilliananya@gmail.com>
2026-07-31 14:07:36 +0530

    Time: 39 ms (5.90%) | Memory: 19.7 MB (33.17%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d={}
+        for i in magazine:
+            if i in d.keys():
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in ransomNote:
+            if i not in d.keys():
+                return False
+            else:
+                if d[i]>0:
+                    d[i]-=1
+                else:
+                    return False
+        return True
