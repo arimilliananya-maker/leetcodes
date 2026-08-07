f3cb45b441e55ee021f105a38d6e33a36bd71589
Ananya <arimilliananya@gmail.com>
2026-08-07 11:49:19 +0530

    Time: 63 ms (38.79%) | Memory: 22.3 MB (6.41%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        zero_count=0
+        left=0
+        right=0
+        maxLength=0
+        for right in range(len(nums)):
+            if nums[right]==0:
+                zero_count+=1
+            #find invalid state until valid shrink
+            while zero_count>k:
+                #shrink()
+                if nums[left]==0:
+                    zero_count-=1
+                left+=1
+            maxLength=max(maxLength,right-left+1)
+        return maxLength
+            
+
