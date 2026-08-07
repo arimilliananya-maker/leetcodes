1873b1b2fe09dd0a587bc98145fb5ba139e34c4a
Ananya <arimilliananya@gmail.com>
2026-08-07 10:38:52 +0530

    Time: 38 ms (64.12%) | Memory: 19.8 MB (42.56%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        #step1=compute the freq of string p
+        d2={}
+        for i in p: d2[i]=d2.get(i,0)+1
+        #step2 : do a klength sliding window on s
+        #count the frequencies of charactors in substring into d1
+        k=len(p)
+        d1={}
+        left=0
+        ans=[]
+        for right in range(len(s)):
+            d1[s[right]]=d1.get(s[right],0)+1#counting freq of substring k
+            if right>=k-1: #checking the validity of window
+                if d1==d2: #comparing hashmaps to check anagrams
+                    ans.append(left)
+            #removing the outgoing element-left
+                d1[s[left]]-=1
+                if d1[s[left]]==0:
+                    d1.pop(s[left])
+                left+=1
+        return ans
